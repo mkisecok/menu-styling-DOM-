8b94cd7475746d7d7adfcbbda38c11aa77ca49e3
Mkisecok <mahmutkisecok@gmail.com>
2021-10-26 19:09:44 +0200

    Add style for food-desc

--- index.js
+++ index.js
@@ -21,57 +21,60 @@ let label = document.querySelectorAll('.category')
  function colorGenerator(element)
 {
     randomNumber = Math.round(Math.random()*100)  
+
     randomColor = document.getElementsByClassName(element)
-    randomColor[0].style.backgroundColor=`#${randomNumber.toString()<10 ? 15 : randomNumber.toString()}15FF `
+    randomColor[0].style.backgroundColor=`#${randomNumber.toString()<10 ? 15 : randomNumber.toString()}10F6 `
     randomColor[1].style.backgroundColor=`#${randomNumber.toString()<10 ? 15 : randomNumber.toString()}1165 `
     randomColor[2].style.backgroundColor=`#${randomNumber.toString()<10 ? 15 : randomNumber.toString()}1298 `
 }
 colorGenerator('food-category')
 
-//  function colorGenerator(element)
-//  {  
-//     let colorPalete=["#0099cc","#c0c0c0","#587b2e","#990000","#000000","#1C8200","#987baa","#981890","#AA8971","#1987FC","#99081E"];
 
-//     let  randomize = Math.floor(Math.random() * colorPalete.length);
-
-//     let selectColor = colorPalete[randomize];
-   
-//       document.querySelectorAll(element).forEach(element=>
-//     {
-//             element.style.backgroundColor=selectColor
-//     })
-
-
-// }
- // colorGenerator('.food-category')
-
- //5
-
- 
-
- 
  let width =window.innerWidth;
 
 
  if (width <= 425)
 {
     document.querySelector('.main').style.display = 'flex';
+
     document.querySelector('.main').style.flexDirection = 'column';
+
     document.querySelector('.main').style.alignItems = 'center';
+
     document.querySelectorAll('.food-category').forEach(element => element.style.width='250px')  
+    
+    document.querySelector('.footer').style.display='flex'
+
+    
+    document.querySelector('.footer').style.flexDirection='column'
+
+    document.querySelector('.footer').style.alignItems='center'
+
+   
 }
 else
 {
     document.querySelector('.main').style.display = 'flex'
+
     document.querySelector('.main').style.justifyContent = 'space-around'
-    console.log(document.querySelectorAll('.food-category'));
+
+    
     document.querySelectorAll('.food-category').forEach(element => element.style.width='30%')
+
     document.querySelectorAll('.food-category').forEach(element => element.style.marginRight='10px')  
+
+    document.querySelector('.footer').style.display='flex'
+
+    document.querySelector('.footer').style.justifyContent = 'space-around'
+
+    document.querySelector('.footer').style.alignItems='center'
+    
+    
 }
 
 //6
 let warn = document.getElementById('warning');
-console.log(warn);
+
 warn.style.fontSize='24px';
 warn.style.fontFamily='Time new roman'
 
@@ -81,10 +84,10 @@ let test=document.querySelectorAll('.allergy-info');
 
 for(let i = 0; i < test.length; i++)
 {
-    //console.log(test[i]);
+    
     if(i%2 != 0)
     {
-        //console.log(test[i]);
+       
          test[i].style.backgroundColor = 'skyBlue'
     }
 }
@@ -104,6 +107,23 @@ document.querySelector('.allergies').style.alignContent='center'
 document.querySelector('.allergies').style.width='50%'
 
 document.querySelectorAll('.allergy-info').forEach(element=> element.style.width = '100%'  )
+
+//9
+document.querySelectorAll('.food-desc').forEach(element=>
+    {
+        element.style.border ='4px solid orange';
+        element.style.borderRadius='50%';
+        element.style.padding='10px';
+        element.style.width='15mw';
+        element.style.margin='10px'
+    }
+    )
+
+
+
+
+
+
 
 
 
